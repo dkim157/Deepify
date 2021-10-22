f80c7649e6d0f063589f6f0d421c8f4a6dd606ff
emilyt <etruon08@calpoly.edu>
2021-10-22 13:50:59 -0700

    changed form to accept an artist name

--- frontend/src/Form.js
+++ frontend/src/Form.js
@@ -4,39 +4,32 @@ function Form(props) {
   const [person, setPerson] = useState(
      {
         name: '',
-        job: '',
+        // job: '',
      }
   );
   function handleChange(event) {
     const { name, value } = event.target;
     if (name === "job")
       setPerson(
-         {name: person['name'], job: value}
+         {name: person['name']}
       );
     else     
        setPerson(
-         {name: value, job: person['job']}   
+         {name: value}   
        );
   }
   function submitForm() {
     props.handleSubmit(person);
-    setPerson({name: '', job: ''});
+    setPerson({name: ''});
   }
   return (
     <form>
-      <label htmlFor="name">Name</label>
+      <label htmlFor="name">Artist Name</label>
       <input
         type="text"
         name="name"
         id="name"
         value={person.name}
-        onChange={handleChange} />
-      <label htmlFor="job">Job</label>
-      <input
-        type="text"
-        name="job"
-        id="job"
-        value={person.job}
         onChange={handleChange} />
       <input type="button" value="Submit" onClick={submitForm} />
     </form>
