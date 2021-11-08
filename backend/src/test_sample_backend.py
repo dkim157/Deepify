50d55c980a2ac6c6d1c141e62538235e0014b5cb
dkim157 <63015027+dkim157@users.noreply.github.com>
2021-11-08 13:27:00 -0800

    test file works

--- backend/src/test_sample_backend.py
+++ backend/src/test_sample_backend.py
@@ -1,11 +1,13 @@
 import pytest
 import sample_backend
+from parent_node import ParentNode
 
 def test_get_artist_success():  
    expected = 'Bladee'
-   
-   assert sample_backend.get_artist("Bladee")['name'] == expected
+   assert ParentNode.get_artist_data('bladee')['name'] == expected
 
 def test_get_artist_fail():  
    expected = []
    assert sample_backend.get_artist("Jeff") == expected
+
+test_get_artist_success()
