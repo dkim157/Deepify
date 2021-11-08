3db116264d357a7aae080ef529df7908d61dec7c
Justin Tran <jhtranx@gmail.com>
2021-11-08 10:50:14 -0800

    travis and test file update

--- backend/src/test_sample_backend.py
+++ backend/src/test_sample_backend.py
@@ -4,8 +4,8 @@ import sample_backend
 def test_get_artist_success():  
    expected = 'Bladee'
    
-   assert sample_backend.User().get_artist("Bladee") == expected
+   assert sample_backend.get_artist("Bladee")['name'] == expected
 
 def test_get_artist_fail():  
    expected = []
-   assert sample_backend.User().find_by_name("Jeff") == expected
+   assert sample_backend.get_artist("Jeff") == expected
